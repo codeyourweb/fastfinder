de6f3fa0efdf596baff79e6c3702dfd76086e895
Jean-Pierre GARNIER <dev.jp.garnier@gmail.com>
2021-12-12 20:11:37 +0100

    1.4 release

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 
 	// version
 	if *pFinderVersion {
-		fmt.Println("fastfinder v1.3")
+		fmt.Println("fastfinder v1.4")
 		if !Contains(os.Args, "-c") && !Contains(os.Args, "--configuration") {
 			os.Exit(0)
 		}
